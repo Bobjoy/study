12ec7bd5850e754943d81add8251c80e298a3d3c
bobjoy <bobjoy1010@163.com>
2015-09-17 16:45:35 +0800

    Yeoman Demo

---
+++ nodejs-in-action/yeoman/mytodo/app/scripts/controllers/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope) {
+
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+
+    // addTodo
+    $scope.addTodo = function() {
+    	$scope.todos.push($scope.todo);
+    	$scope.todo = '';
+    };
+
+    // removeTodo
+    $scope.removeTodo = function(index) {
+    	$scope.todos.splice(index, 1);
+    };
+  });
