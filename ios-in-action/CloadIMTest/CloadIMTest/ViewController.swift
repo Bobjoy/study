7f9216c15444f706ac3c807f1274d37cb9c5c16e
Vetech <vetech@VetechdeMac-mini.local>
2015-09-22 13:44:16 +0800

    Update

---
+++ ios-in-action/CloadIMTest/CloadIMTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloadIMTest
+//
+//  Created by Vetech on 15/9/22.
+//  Copyright (c) 2015年 BFL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
