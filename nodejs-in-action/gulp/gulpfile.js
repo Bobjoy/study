a23486d3f71af45c848035bb4ed505274bf6695c
bobjoy <bobjoy1010@163.com>
2015-09-29 18:31:30 +0800

    Update

--- nodejs-in-action/gulp/gulpfile.js
+++ nodejs-in-action/gulp/gulpfile.js
@@ -33,3 +33,28 @@ gulp.task('build', ['css', 'js'], function(){
 gulp.task('default', ['build'], function(){
 	console.log('default');
 });
+
+gulp.task('watch', function(){
+	gulp.watch('templates/*.tmpl.html', function(event){
+		console.log('Event type: ' + event.type);
+		console.log('Event path: ' + event.path);
+	});
+});
+
+var watcher = gulp.watch('templates/*.tmpl.html', ['build']);
+watcher.on('change', function(event){
+	console.log('Event type: ' + event.type);
+	console.log('Event path: ' + event.path);
+});
+
+var less = require('gulp-less'),
+	livereload = require('gulp-livereload'),
+	watch = require('gulp-watch');
+
+gulp.task('less', function(){
+	gulp.src('less/*.less')
+		.pipe(watch())
+		.pipe(less())
+		.pipe(gulp.dest('css'))
+		.pipe(livereload())
+});
