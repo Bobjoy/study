8b2bbd0995b16e986bae9daa0ff1a30c51023924
bobjoy <bobjoy1010@163.com>
2015-10-13 08:26:37 +0800

    ios demo

---
+++ ios-in-action/iOSStudy-Swift2.0/iOSStudy-Swift2.0/面向协议编程/SwiftProtocols.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftProtocols.swift
+//  iOSStudy-Swift2.0
+//
+//  Created by Vetech on 15/10/9.
+//  Copyright © 2015年 BFL. All rights reserved.
+//
+
+import Cocoa
+
+class SwiftProtocols: NSObject {
+
+}
